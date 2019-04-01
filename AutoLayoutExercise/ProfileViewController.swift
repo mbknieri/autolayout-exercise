3ce24a63cf787313c787a3ea2a0119764fccfeb5
Marissa Moore <mmoore2@wayfair.com>
2019-04-01 15:10:18 -0400

    Reset to cleaned up code

--- AutoLayoutExercise/ProfileViewController.swift
+++ AutoLayoutExercise/ProfileViewController.swift
@@ -28,13 +28,11 @@ class ProfileViewController: UIViewController {
     
     private var profilePic: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "profile_pic"))
-        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
     private var nameLabel: UILabel = {
         let label = UILabel(frame: .zero)
-        label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "John Doe"
         label.font = UIFont.systemFont(ofSize: Constants.nameLabelSize, weight: .semibold)
         return label
@@ -42,21 +40,18 @@ class ProfileViewController: UIViewController {
     
     private var statusStackView: UIStackView = {
         let stackView = UIStackView(frame: .zero)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.spacing = Constants.statusStackViewSpacing
         return stackView
     }()
     
     private var hometownStackView: UIStackView = {
         let stackView = UIStackView(frame: .zero)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.spacing = Constants.hometownStackViewSpacing
         return stackView
     }()
     
     private var bioStackView: UIStackView = {
         let stackView = UIStackView(frame: .zero)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.alignment = .top
         stackView.spacing = Constants.bioStackViewSpacing
         return stackView
@@ -64,7 +59,6 @@ class ProfileViewController: UIViewController {
     
     private var mainStackView: UIStackView = {
         let stackView = UIStackView(frame: .zero)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = Constants.mainStackViewSpacing
         return stackView
@@ -98,24 +92,7 @@ private extension ProfileViewController {
         mainStackView.addArrangedSubview(hometownStackView)
         mainStackView.addArrangedSubview(bioStackView)
         
-        //Add constraints
-        NSLayoutConstraint.activate([
-            //Profile Pic Constraints
-            profilePic.widthAnchor.constraint(equalTo: profilePic.heightAnchor, multiplier: Constants.profilePicAspectRatio),
-            profilePic.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: Constants.profilePicHeightRatio),
-            profilePic.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            profilePic.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            //Name Label Constraints
-            nameLabel.topAnchor.constraint(equalTo: profilePic.bottomAnchor, constant: Constants.profilePicToNameLabelSpacing),
-            nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            //Main StackView Constraints
-            mainStackView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: Constants.nameLabelToMainStackViewSpacing),
-            mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: Constants.mainStackViewLeadingSpacing),
-            mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: Constants.mainStackViewTrailingSpacing),
-            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: Constants.mainStackViewBottomSpacing)
-        ])
+        //TODO: Add constraints
     }
     
     func setupStatusStackView() {
@@ -169,7 +146,7 @@ private extension ProfileViewController {
         userBioLabel.text = "I love to travel and see amazing places! I have been to London, Tokyo, Sydney, and Rome. Check out the photos I have taken in my albums!"
         userBioLabel.font = UIFont.systemFont(ofSize: Constants.labelFontSize, weight: .light)
         userBioLabel.numberOfLines = 0
-        userBioLabel.lineBreakMode = .byWordWrapping //default is .byTruncatingTail
+        userBioLabel.lineBreakMode = .byWordWrapping
         userBioLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         userBioLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
         
